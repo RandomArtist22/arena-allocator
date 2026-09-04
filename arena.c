4d83e3055ad44e2a1bbb0e8bcb26023b21abc6d6
RandomArtist22 <ytanimator123@gmail.com>
2026-09-05 05:21:41 +0530

    Added arena allocator functions

--- arena.c
+++ arena.c
@@ -1,5 +1,6 @@
 #include "arena.h"
 #include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
@@ -20,4 +21,24 @@ uintptr_t align_forward(uintptr_t ptr, size_t align) {
         p += a - modulo;
     }
     return p;
+}
+
+void *arena_alloc_align(Arena *a, size_t size, size_t align) {
+    uintptr_t curr_ptr = (uintptr_t)a->buf + (uintptr_t)a->curr_offset;
+    uintptr_t offset = align_forward(curr_ptr, align);
+    offset -= (uintptr_t)a->buf;
+
+    if (offset + size <= a->buf_len) {
+        void *ptr = &a->buf[offset];
+        a->prev_offset = offset;
+        a->curr_offset = offset + size;
+
+        memset(ptr, 0, size);
+        return ptr;
+    }
+    return NULL;
+}
+
+void *arena_alloc(Arena *a, size_t size) {
+    arena_alloc_align(a, size, DEFUALT_ALIGNMENT);
 }
--- arena.h
+++ arena.h
@@ -5,7 +5,21 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef struct Arena Arena;
+struct Arena {
+    unsigned char *buf;
+    size_t buf_len;
+    size_t prev_offset;
+    size_t curr_offset;
+};
+
+#ifndef DEFUALT_ALIGNMENT
+#define DEFUALT_ALIGNMENT (2 * sizeof(void *))
+#endif
+
 bool is_power_of_two(uintptr_t x);
 uintptr_t align_forward(uintptr_t ptr, size_t align);
+void *arena_alloc_align(Arena *a, size_t size, size_t align);
+void *arena_alloc(Arena *a, size_t size);
 
 #endif
